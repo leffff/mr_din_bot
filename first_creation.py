b7f31c63b479bbe0f8a84890623a7975ae892e02
semeka13 <semeka13@yandex.ru>
2020-06-06 21:31:51 +1000

    first_creation.py added

--- first_creation.py
+++ first_creation.py
@@ -9,54 +9,48 @@ with open('config.json') as f:
     USER = config["USER"]
     HOST = config["HOST"]
     PORT = config["PORT"]
-
-
 # Первое создание базы данных
-def first_db_creation():
-    '''
-    return {"result": "ok"}
-    return {"result": "database already exists"}
-    return {"result": "unknown error"}
-    '''
+'''
+Первое создание базы данных.
+return {"result": "ok"}
+return {"result": "database already exists"}
+return {"result": "unknown error"}
+'''
 
-    conn = psycopg2.connect(dbname=DBNAME, user=USER,
-                            password=PASSWORD, host=HOST, port=PORT)
-    cursor = conn.cursor()
+
+def first_db_creation():
     try:
-        cursor.execute("CREATE TABLE users ("
-                       "user_id INT PRIMARY KEY NOT NULL, "
-                       "tg_nickname VARCHAR(30) NOT NULL UNIQUE, "
-                       "name VARCHAR(30) NOT NULL, "
-                       "surname VARCHAR(30) NOT NULL, "
-                       "qualification text NOT NULL, "
-                       "experience INT NOT NULL, "
-                       "qualities text NOT NULL,"
-                       "city VARCHAR(30) NOT NULL)")
-        conn.commit()
-        cursor.execute("CREATE TABLE cancellations ("
-                       "cancellation_id SERIAL PRIMARY KEY NOT NULL, "
-                       "employer_self BOOL DEFAULT FALSE, "
-                       "employer_other BOOL DEFAULT FALSE, "
-                       "worker_self BOOL DEFAULT FALSE, "
-                       "worker_other BOOL DEFAULT FALSE)")
-        conn.commit()
-        cursor.execute("CREATE TABLE orders ("
-                       "order_id SERIAL PRIMARY KEY NOT NULL, "
-                       "employer_id SERIAL REFERENCES users(user_id) NOT NULL,"
-                       "worker_id SERIAL REFERENCES users(user_id),"
-                       "title VARCHAR(40) NOT NULL, "
-                       "description TEXT NOT NULL, "
-                       "payment REAL NOT NULL, "
-                       "active BOOLEAN DEFAULT True NOT NULL, "
-                       "start_time INT, "
-                       "finish_time INT,"
-                       "mark INT,"
-                       "feedback TEXT,"
-                       "cancellation_id SERIAL REFERENCES cancellations(cancellation_id))")
-        conn.commit()
-    except psycopg2.errors.DuplicateTable:
-        return {"result": "database already exists"}
+        with psycopg2.connect(dbname=DBNAME, user=USER,
+                              password=PASSWORD, host=HOST, port=PORT) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("CREATE TABLE users ("
+                               "user_id SERIAL PRIMARY KEY NOT NULL, "
+                               "tg_nickname VARCHAR(30) NOT NULL UNIQUE, "
+                               "name VARCHAR(30) NOT NULL, "
+                               "surname VARCHAR(30) NOT NULL, "
+                               "qualification text NOT NULL, "
+                               "experience INT NOT NULL, "
+                               "qualities text NOT NULL,"
+                               "city VARCHAR(30) NOT NULL);")
+                cursor.execute("CREATE TABLE orders ("
+                               "order_id SERIAL PRIMARY KEY NOT NULL, "
+                               "employer_id SERIAL REFERENCES users(user_id) NOT NULL,"
+                               "worker_id SERIAL REFERENCES users(user_id),"
+                               "title VARCHAR(40) NOT NULL, "
+                               "description TEXT NOT NULL, "
+                               "payment REAL NOT NULL, "
+                               "active BOOLEAN DEFAULT True NOT NULL, "
+                               "done BOOLEAN DEFAULT False NOT NULL, "
+                               "start_time INT,"
+                               "finish_time INT,"
+                               "mark INT,"
+                               "feedback TEXT,"
+                               "cancellation VARCHAR(30),"
+                               "result INT,"
+                               "active_orders INT)")
     except Exception as ex:
-        return {"result": f"unknown error: {ex.args[0]}"}
+        return {"status": f"unknown error: {ex.args[0]}"}
     else:
-        return {"result": "ok"}
+        return {"status": "ok"}
+    finally:
+        conn.close()
